d1b4a3233410e7eb4a5ceb209ff6f420daa9a36d
Akshara Kumar <93akshara@gmail.com>
2018-01-02 18:27:46 -0600

    SQL Introuction and Basic Queries

---
+++ SQL_Databases_Beginner/Guided Project_ Working With a SQLite Database-155.sh
@@ -0,0 +1,33 @@
+## 1. Overview of the Data Set ##
+
+
+
+
+## 2. Overview of the SQLite Command Shell ##
+
+
+
+
+## 3. Running Queries in the SQLite Command Shell ##
+
+
+
+
+## 4. Using Python with SQLite ##
+
+
+
+
+## 5. Computing Population Projections ##
+
+
+
+
+## 6. Summing Columns to Compute Total Area ##
+
+
+
+
+## 7. Next Steps ##
+
+
