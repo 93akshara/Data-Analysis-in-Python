02a0f3595981d6e4626feb449ceff66f44c2a620
Akshara Kumar <93akshara@gmail.com>
2018-01-05 20:52:33 -0600

    SQL Intermediate Level Schemas and Queries

---
+++ Sql_Databases_Intermediate/Guided Project_ Creating relations in SQLite-188.sh
@@ -0,0 +1,33 @@
+## 1. Introduction to the data ##
+
+
+
+
+## 2. Creating the ceremonies table ##
+
+
+
+
+## 3. Foreign key constraints ##
+
+
+
+
+## 4. Setting up one-to-many ##
+
+
+
+
+## 5. Deleting and renaming tables ##
+
+
+
+
+## 6. Creating a join table ##
+
+
+
+
+## 7. Next steps ##
+
+
