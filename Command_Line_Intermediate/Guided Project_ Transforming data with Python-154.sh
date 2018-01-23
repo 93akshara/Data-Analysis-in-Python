bdf2d837cd7b7edb10d4d051e8d4e742ca9563ce
Akshara Kumar <93akshara@gmail.com>
2018-01-22 20:00:48 -0600

    Data Cleaning, Exploration, Piping

---
+++ Command_Line_Intermediate/Guided Project_ Transforming data with Python-154.sh
@@ -0,0 +1,33 @@
+## 1. How guided projects work ##
+
+
+
+
+## 2. The dataset ##
+
+
+
+
+## 3. Reading the data ##
+
+
+
+
+## 4. Which words appear in the headlines often? ##
+
+
+
+
+## 5. Which domains were submitted most often? ##
+
+
+
+
+## 6. When are the most articles submitted? ##
+
+
+
+
+## 7. Next steps ##
+
+
